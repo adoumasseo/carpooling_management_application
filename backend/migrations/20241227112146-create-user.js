b99cd2189d781797cd1508ad6b22f25536016652
adoumasseo <adoumasseo@gmail.com>
2024-12-30 08:31:46 +0100

    feat: Company and user models and migrations whitout association between them

---
+++ backend/migrations/20241227112146-create-user.js
@@ -0,0 +1,46 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true, // Ensure email uniqueness at the database level
+      },
+      role: {
+        type: Sequelize.ENUM('super_admin', 'company_admin', 'user'),
+        allowNull: false,
+        defaultValue: 'user',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    // Drop ENUM type explicitly to avoid errors when recreating the table
+    await queryInterface.dropTable('Users');
+    await queryInterface.sequelize.query('DROP TYPE IF EXISTS "enum_Users_role"');
+  },
+};
---
+++ backend/models/user.js
@@ -0,0 +1,99 @@
+'use strict';
+const bcrypt = require('bcrypt'); // Use bcrypt for password hashing
+const { Model } = require('sequelize');
+const validator = require('validator')
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+
+    /**
+     * Instance method to verify password.
+     * @param {string} password Plain text password to verify.
+     * @returns {Promise<boolean>}
+     */
+    async isValidPassword(password) {
+      try {
+        return await bcrypt.compare(password, this.password);
+      } catch (err) {
+        console.error('Error while comparing password:', err);
+        return false;
+      }
+    }
+  }
+
+  User.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+          len: [2, 50],
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+       
+        validate: {
+          isStrongPassword(value) {
+            if (!validator.isStrongPassword(value, {
+              minLength: 8,
+              minLowercase: 1,
+              minUppercase: 1,
+              minNumbers: 1,
+              minSymbols: 1,
+            })) {
+              throw new Error('Password must include at least 8 characters, one lowercase, one uppercase, one number, and one special character.');
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      role: {
+        type: DataTypes.ENUM('super_admin', 'company_admin', 'user'),
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    }
+  );
+  User.beforeCreate(async (user) => {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(user.password, salt);
+    } catch (e) {
+      throw new Error('Error while hashing password.'); 
+    }
+  });
+  
+  User.beforeUpdate(async (user) => {
+    if (user.changed('password')) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      } catch (e) {
+        throw new Error('Error while hashing password.');
+      }
+    }
+  });
+  
+  return User;
+};
