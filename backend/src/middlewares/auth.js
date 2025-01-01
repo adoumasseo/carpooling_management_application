30f96ffe1147cf6b9055f90b569c5d37ca29da24
adoumasseo <adoumasseo@gmail.com>
2025-01-01 19:13:57 +0100

    feat: Auth middleware to verify the user token

---
+++ backend/src/middlewares/auth.js
@@ -0,0 +1,20 @@
+const { verifyToken } = require('../../config/jwt');
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+ 
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ error: 'Authorization token missing or malformed' });
+  }
+  token = authHeader.split(' ')[1];
+  
+  try {
+    const user = verifyToken(token, 'ACCESS_TOKEN');
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Unauthorized: Invalid or expired token' });
+  }
+}
+
+module.exports = authenticateToken;
