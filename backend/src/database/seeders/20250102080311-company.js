7a25734b4b9b7921f03e8e733bfb9e121b097f6e
adoumasseo <adoumasseo@gmail.com>
2025-01-02 09:36:34 +0100

    config: fix /src/database/config/config.js to call .env variable and add seeder for companies and users tables

--- backend/src/database/seeders/20250102080311-company.js
+++ backend/src/database/seeders/20250102080311-company.js
@@ -17,13 +17,17 @@ module.exports = {
         id: '95c47a68-f724-483a-921a-4d8aefd86c8b',
         name: 'MTCA',
         address: 'Haie Vive',
-        email: 'mtcasp@gouv.bj'
+        email: 'mtcasp@gouv.bj',
+        createdAt: '2018-03-22 08:30:58.7',
+        updatedAt: '2018-03-22 08:30:58.7',
       },
       {
         id: '10d5689d-6027-4623-a879-7aeec6d05540',
         name: 'ENEAM',
         address: 'Gbegamey',
-        email: 'eneam@gouv.bj'
+        email: 'eneam@gouv.bj',
+        createdAt: '2018-03-22 08:30:58.7',
+        updatedAt: '2018-03-22 08:30:58.7',
       }
     ], {})
   },
--- backend/src/database/seeders/20250102080457-admin-user.js
+++ backend/src/database/seeders/20250102080457-admin-user.js
@@ -18,13 +18,19 @@ module.exports = {
         name: 'AdminAdmin',
         password: 'admin@admin',
         email: 'adoumasseo@gmail.com',
-        role: 'super_admin'
+        role: 'super_admin',
+        createdAt: '2018-03-22 08:30:58.7',
+        updatedAt: '2018-03-22 08:30:58.7',
       },
       {
         id: '10d5689d-6027-4623-a879-7aeec6d05540',
-        name: 'ENEAM',
-        address: 'Gbegamey',
-        email: 'eneam@gouv.bj'
+        name: 'Ortniel',
+        password: 'Ortniel@Ortniel',
+        email: 'adoumasseo@ortniel.bj',
+        companyId: '95c47a68-f724-483a-921a-4d8aefd86c8b',
+        role: 'user',
+        createdAt: '2018-03-22 08:30:58.7',
+        updatedAt: '2018-03-22 08:30:58.7',
       }
     ], {})
   },
@@ -36,5 +42,6 @@ module.exports = {
      * Example:
      * await queryInterface.bulkDelete('People', null, {});
      */
+     await queryInterface.bulkDelete('Users', null, {});
   }
 };
